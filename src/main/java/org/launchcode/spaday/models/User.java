0fba0cbceb9f8df81e4d3d795669154e7b0379eb
rebaderbs <rderbak@gmail.com>
2022-10-19 20:05:22 -0500

    chapter 15 studio completed up to bonus mission

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,36 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank
+    @NotNull
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters long")
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @NotNull
+    @Size(min=6, message="Password must be at least 6 characters long")
     private String password;
 
+    @NotBlank
+    @NotNull
+    private String verify;
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +55,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
